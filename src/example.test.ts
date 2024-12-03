1390dfba665d1d72157434708cced9874da7c14c
Léonard Binet <leonard.binet@traace.co>
2024-12-03 15:08:17 +0100

    repro

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,32 +1,80 @@
-import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/sqlite';
+import {
+  Entity,
+  helper,
+  LoadStrategy,
+  ManyToOne,
+  MikroORM,
+  PostgreSqlDriver,
+  PrimaryKey,
+  ref,
+  Ref
+} from '@mikro-orm/postgresql';
 
-@Entity()
-class User {
+import {Type} from '@mikro-orm/core'
 
-  @PrimaryKey()
-  id!: number;
-
-  @Property()
-  name: string;
-
-  @Property({ unique: true })
-  email: string;
-
-  constructor(name: string, email: string) {
-    this.name = name;
-    this.email = email;
+export class IDStoredAsInteger extends Type<string, number> {
+  convertToDatabaseValue(value: string): number {
+    return parseInt(value)
   }
 
+  convertToJSValue(value: number): string {
+    return `${value}`
+  }
+
+  getColumnType() {
+    return `integer`
+  }
+
+  // FIXES the issue
+  // compareValues(a: number, b: number): boolean {
+  //   return a.toString() === b.toString()
+  // }
+
 }
+
+
+@Entity()
+class AnalysisGroup {
+  @PrimaryKey({type: IDStoredAsInteger, autoincrement: true})
+  readonly id!: string;
+}
+
+@Entity()
+class Analysis {
+  @PrimaryKey({type: IDStoredAsInteger, autoincrement: true})
+  readonly id!: string;
+
+  @ManyToOne(() => AnalysisGroup, { fieldName: 'analysisGroupId', deleteRule: 'NO ACTION', hidden: true })
+  public analysisGroup!: Ref<AnalysisGroup>
+}
+
+@Entity()
+class Trajectory {
+  @PrimaryKey({type: IDStoredAsInteger, autoincrement: true})
+  readonly id!: string;
+
+  @ManyToOne(() => Analysis, { deleteRule: 'CASCADE', fieldName: 'referenceAnalysisId', index: true })
+  referenceAnalysis!: Ref<Analysis>
+}
+
 
 let orm: MikroORM;
 
 beforeAll(async () => {
   orm = await MikroORM.init({
-    dbName: ':memory:',
-    entities: [User],
+    dbName: 'test',
+    host: 'localhost',
+    port: 5453,
+    user: "postgres_user",
+    password: "postgres_password",
+    entities: [Analysis, AnalysisGroup, Trajectory],
     debug: ['query', 'query-params'],
-    allowGlobalContext: true, // only for testing
+    allowGlobalContext: true, // only for testing,
+    driver: PostgreSqlDriver,
+    loadStrategy: LoadStrategy.SELECT_IN,
+    serialization: {
+      forceObject: true
+    },
   });
   await orm.schema.refreshDatabase();
 });
@@ -36,16 +84,24 @@ afterAll(async () => {
 });
 
 test('basic CRUD example', async () => {
-  orm.em.create(User, { name: 'Foo', email: 'foo' });
+  const analysisGroup1 = orm.em.create(AnalysisGroup, {});
+  const analysis = orm.em.create(Analysis, { analysisGroup: ref(analysisGroup1) });
+  const trajectory = orm.em.create(Trajectory, { referenceAnalysis: ref(analysis) });
+
   await orm.em.flush();
   orm.em.clear();
 
-  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
-  expect(user.name).toBe('Foo');
-  user.name = 'Bar';
-  orm.em.remove(user);
+  // KO; in this order, the update is triggered in my codebase, but this is not the case here
+  const koFetchedTrajectory = await orm.em.findOneOrFail(Trajectory, { id: trajectory.id });
+  const koFetchedAnalysis = await orm.em.findOneOrFail(Analysis, { id: analysis.id });
+  console.log("fetchedAnalysis", helper(koFetchedAnalysis).__originalEntityData)
   await orm.em.flush();
+  orm.em.clear();
 
-  const count = await orm.em.count(User, { email: 'foo' });
-  expect(count).toBe(0);
+  // OK: in this order, the update isn't triggered
+  const okFetchedAnalysis = await orm.em.findOneOrFail(Analysis, { id: analysis.id });
+  const okFetchedTrajectory = await orm.em.findOneOrFail(Trajectory, { id: trajectory.id });
+  await orm.em.flush();
+  orm.em.clear();
+
 });
